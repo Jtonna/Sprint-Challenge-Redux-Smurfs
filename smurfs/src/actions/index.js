4bfe30fb00f57c8cda7fe75909335c64e158502a
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-17 11:46:30 -0700

    finished actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -16,7 +16,7 @@ export const addSmurf = newSmurf => dispatch => {
     .post(`http;//localhost:3333/smurfs`, newSmurf)
 
     .then(response => {
-      console.log('src/actions/index.jsx says:', response.data);
+      console.log('src/actions/index.jsx addSmurf says:', response.data);
       dispatch({
         type: ADD_SMURF,
         payload: response.data,
@@ -41,7 +41,7 @@ export const getSmurfs = () => dispatch => {
     .get(`http://localhost:3333/smurfs`)
 
     .then(response => {
-      console.log('src/actions/index.jsx says:', response.data)
+      console.log('src/actions/index.jsx getSmurf says:', response.data)
       dispatch ({
         type: GET_SMURFS,
         payload: response.data,
@@ -64,7 +64,7 @@ export const deleteSmurf = id => dispatch => {
   axios
     .delete(`http://localhost:3333/smurfs/${id}`)
     .then(response => {
-      console.log('src/actions/index.jsx says:', response.data)
+      console.log('src/actions/index.jsx deleteSmurf says:', response.data)
       dispatch({
         type: DELETE_SMURF,
         payload: err.errorMessage,
@@ -74,6 +74,28 @@ export const deleteSmurf = id => dispatch => {
       dispatch({
         type: ERROR,
         payload: error.errorMessage,
+      })
+    })
+}
+
+export const updateSmurf = updatedSmurf => dispatch => {
+  dispatch({
+    type: LOADING
+  })
+
+  axios
+    .put(`http://localhost:3333/smurfs/${updatedSmurf.id}`, updatedSmurf)
+    .then(response => {
+      console.log('src/actions.index.jsx updateSmurf says:', response.data)
+      dispatch({
+        type: UPDATE_SMURF,
+        payload: response.data,
+      })
+    })
+    .catch (err => {
+      dispatch({
+        type: ERROR,
+        payload: err.errorMessage
       })
     })
 }
