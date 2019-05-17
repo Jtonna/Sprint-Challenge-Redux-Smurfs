7223e1ed473bec5b771ac8eaeb4da6ccc6e76d01
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-17 11:15:23 -0700

    Added an extra smurf

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ let smurfs = [
     name: 'Brainey',
     age: 200,
     height: '5cm',
-    id: 0
+    id: 0,
+  },
+  {
+    name: 'Jacob',
+    age: 20,
+    height: '187.452cm',
+    id: 1,
   }
 ];
 server.get('/smurfs', (req, res) => {
