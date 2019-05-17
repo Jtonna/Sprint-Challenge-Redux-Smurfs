dda9faffb725bb2cff173b29813824d1fe9f254a
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-17 11:42:11 -0700

    adding actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,10 +1,5 @@
 import axios from 'axios';
 
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
-
 export const ADD_SMURF = 'ADD_SMURF';
 export const GET_SMURFS = 'GET_SMURFS';
 export const UPDATE_SMURF = 'UPDATE_SMURF';
@@ -12,28 +7,41 @@ export const DELETE_SMURF = 'DELETE_SMURF';
 export const ERROR = 'ERROR';
 export const LOADING = 'LOADING';
 
+export const addSmurf = newSmurf => dispatch => {
+  dispatch({
+    type: LOADING,
+  })
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+  axios
+    .post(`http;//localhost:3333/smurfs`, newSmurf)
+
+    .then(response => {
+      console.log('src/actions/index.jsx says:', response.data);
+      dispatch({
+        type: ADD_SMURF,
+        payload: response.data,
+      })
+    })
+
+    .catch(err => {
+      dispatch({
+        type: ERROR,
+        payload: err.errorMessage,
+      })
+    })
+
+}
 
 export const getSmurfs = () => dispatch => {
   dispatch({
-    type: LOADING,
+    type: LOADING
   })
 
   axios
     .get(`http://localhost:3333/smurfs`)
 
     .then(response => {
-      console.log('src/actions/index.jsx says:'. response.data)
+      console.log('src/actions/index.jsx says:', response.data)
       dispatch ({
         type: GET_SMURFS,
         payload: response.data,
@@ -46,5 +54,26 @@ export const getSmurfs = () => dispatch => {
         payload: err.errorMessage,
       })
     })
-     
+};
+
+export const deleteSmurf = id => dispatch => {
+  dispatch({
+    type: LOADING
+  })
+
+  axios
+    .delete(`http://localhost:3333/smurfs/${id}`)
+    .then(response => {
+      console.log('src/actions/index.jsx says:', response.data)
+      dispatch({
+        type: DELETE_SMURF,
+        payload: err.errorMessage,
+      })
+    })
+    .catch(err =>{
+      dispatch({
+        type: ERROR,
+        payload: error.errorMessage,
+      })
+    })
 }
